76531b88381e8986ee2241093f9727a3559fa2aa
Sebastian Zazzi <sebastian@zazzi.se>
2020-11-09 12:35:44 +0100

    added burger meny and styling

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 window.addEventListener(onload, loadFunctions());
 
 function loadFunctions() {
-    console.log('tjena bois')
+    console.log('Im sorry Dave, Im afraid i cant do that.');
+    getBurger();
+}
+
+
+/**
+ * Burger menu
+ */
+function getBurger() {
+    const burger = document.querySelector('#burger');
+
+    burger.addEventListener('click', openBurger);
+}
+
+function openBurger() {
+    const navbar = document.querySelector('#innerBurger');
+    const burger = document.querySelector('#burger');
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer')
+
+    navbar.classList.toggle("showBurgerNav");
+    burger.classList.toggle("closedBurgerNav");
+    main.classList.toggle('blurEffects');
+    footer.classList.toggle('blurEffects')
 }
