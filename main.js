325e5a918dea5cb45790c1d65749260f112b6789
Sebastian Zazzi <sebastian@zazzi.se>
2020-11-11 16:09:19 +0100

    changed some titles, and started on the life page

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', loadFunctions);
 function loadFunctions() {
     getBurger();
     getGameBtn();
-    gameStart();
+
 }
 
 
@@ -37,8 +37,6 @@ function openBurger() {
 function getGameBtn() {
     const gameBtn = document.getElementById('gameBtn');
     gameBtn.addEventListener('click', gameStart);
-
-
 }
 /**
  * The main game function, here is were the whole game is
@@ -48,10 +46,13 @@ function gameStart() {
     const obstacle = document.getElementById('obstacle');
     const gameDiv = document.getElementById('game');
     let counter = 0;
+    obstacle.classList.add('animateObstacle');
     gameDiv.addEventListener('click', jump); //eventlistener for adding the jump to the jumper
 
     /**
      * Function for adding the jumping animation
+     * if animateJump is already on classlist, cant add list 
+     * then sets timeout and removes the class every 500ms
      */
     function jump() {
         if (jumper.classList != 'animateJump') {
@@ -60,27 +61,31 @@ function gameStart() {
         setTimeout(function () {
             jumper.classList.remove('animateJump');
         }, 500)
-
-
-        console.log('jump')
     }
     /**
      * interval for checking if jumper misses the jump (death)
      */
-    setInterval(() => {
+    const dead = setInterval(() => {
         const jumperTop = parseInt(window.getComputedStyle(jumper).getPropertyValue('top')); //gets the top value for the jumper from the css (parseInt returns the value as an int, removes the rem)
         const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left')); //gets the left value for the obstacle from the css (parseInt returns the value as an int, removes the rem)
 
-        if(obstacleLeft < 32 && obstacleLeft>-32 && jumperTop >=73 ) {
-            console.log('dead')
-            console.log(obstacleLeft)
-            console.log(jumperTop)
-            obstacle.style.animation = "none";
-            obstacle.style.display = "none";   
+        /**
+         * runs and interval and checks the jumpers top value and obstacle left value to check for death.
+         * if dead clears the interval and removes the obstacle animation
+         * if player is not dead adds score to the counter and continues interval
+         */
+        if (obstacleLeft < 32 && obstacleLeft > -32 && jumperTop >= 73) {
+            console.log('dead');
+            console.log(obstacleLeft);
+            console.log(jumperTop);
+            obstacle.classList.remove('animateObstacle');
+            counter = 0;
+            clearInterval(dead);
         } else {
             counter++;
+            document.getElementById('score').innerHTML = counter;
         }
-    }, 20);
+    }, 50);
 
 }
 
