48b98a42edc66f88a6ee685fc9452de22ffdfa69
Sebastian Zazzi <sebastian@zazzi.se>
2020-11-13 18:47:35 +0100

    added a image swapper button in js

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ window.addEventListener('DOMContentLoaded', loadFunctions);
 
 function loadFunctions() {
     getBurger();
-
-    /*
-     *only runs the appearing text on the index page 
-     */
-    if (window.location.href.indexOf('/') > 0) {
-        textAppear();
-    }
 }
 
 
@@ -38,31 +31,4 @@ function openBurger() {
 
 
 
-//#region Appearing text
 
-/**
- * function to make text appear on the DOM as if its being type out.
- */
-function textAppear() {
-    let i = 0;
-    const textToBeInjected = 'Hi. I am Front End Developer in learning.'
-    const speed = 60;
-
-    writingText();
-
-
-    /**
-     * Function to inject the text as in if being typed
-     * The if statement in this function adds the content of textToBeInjected to the h2 element. 
-     * To get the effect of the text being typed, the character with the value of (i) is chosen to be printed, the function then adds 1 to i and runs again. 
-     */
-    function writingText() {
-        if (i < textToBeInjected.length) {
-            document.getElementById('textField').innerHTML += textToBeInjected.charAt(i); // charAt(i) is used to choose the symbol with the value i from the string textToBeInjected
-            i++
-            setTimeout(writingText, speed);
-        }
-    }
-}
-
-//#endregion Appearing text
